c26914608cc423470cdac2106f8db16a545a45c9
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-02-01 20:52:26 +0000

    updated template to use variable, extended FT to check for second to-do

--- lists/tests.py
+++ lists/tests.py
@@ -12,8 +12,15 @@ from pytest_django.asserts import assertTemplateUsed
 def test_home_page_returns_correct_html(client):
     response = client.get("/")
 
-    print('the test is running')
+    print("the test is running")
     assertTemplateUsed(response, "home.html")
+
+
+def test_can_save_a_POST_requrest(client):
+    response = client.post("/", data={"item_text": "A new list item"})
+    assert "A new list item" in response.content.decode()
+    assertTemplateUsed(response, "home.html")
+
 
 @pytest.mark.xfail
 def test_azure_pipeline_fails_with_bad_test():
--- test_functional.py
+++ test_functional.py
@@ -30,19 +30,31 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
 
     table = browser.find_element_by_id("id_list_table")
     rows = table.find_elements_by_tag_name("tr")
-    assert any(
-        row.text == "1: Buy peacock feathers" for row in rows
-    ), "New to-do item did not appear in table"
+    assert "1: Buy peacock feathers" in [
+        row.text for row in rows
+    ], f"New to-do item did not appear in table. Contents were:\n{table.text}"
 
     # There is still a text box inviting her to add another item. She
     # enters 'Use peacock feathers to make a fly' (Edith is very methodical)
-    assert 0, "Finish the test!"
+    inputbox = browser.find_element_by_id("id_new_item")
+    inputbox.send_keys("Use peacock feathers to make a fly")
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
 
     # The page updates again, and now shows both items on her list
+    table = browser.find_element_by_id("id_list_table")
+    rows = table.find_elements_by_tag_name("tr")
+    assert "1: Buy peacock feathers" in [
+        row.text for row in rows
+    ], f"New to-do item did not appear in table. Contents were:\n{table.text}"
+    assert "2: Use peacock feathers to make a fly" in [
+        row.text for row in rows
+    ], f"New to-do item did not appear in table. Contents were:\n{table.text}"
 
     # Edith wonders whether the site will remember her list. The she sees
     # that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
+    assert 0, "Finish the test!"
 
     # she visits that URL - her to-do list is still there.
 
