14c663a4b9a5c20d2bdbe528d1cf2eaac388b29a
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-02-01 21:16:14 +0000

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,12 @@
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from pytest_django.asserts import assertTemplateUsed
 
 import pytest
 
 from lists.views import home_page
-
-from pytest_django.asserts import assertTemplateUsed
+from lists.models import Item
 
 
 def test_home_page_returns_correct_html(client):
@@ -20,6 +20,22 @@ def test_can_save_a_POST_requrest(client):
     response = client.post("/", data={"item_text": "A new list item"})
     assert "A new list item" in response.content.decode()
     assertTemplateUsed(response, "home.html")
+
+
+@pytest.mark.django_db
+def test_saving_and_retrieving_items():
+    first_item = Item()
+    first_item.text = "The first (ever) list item"
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = "Item the second"
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+    assert saved_items[0].text == "The first (ever) list item"
+    assert saved_items[1].text == "Item the second"
 
 
 @pytest.mark.xfail
