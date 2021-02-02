08aca90649e04d968e2fdf3cbbf26777143b71e5
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-02-02 14:21:13 +0000

    added list model

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 import pytest
 from pytest_django.asserts import assertTemplateUsed, assertContains, assertRedirects
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Home view
@@ -10,20 +10,30 @@ def test_home_page_returns_correct_html(client):
     assertTemplateUsed(response, "home.html")
 
 
-# Item model
+# List and Item model
 def test_saving_and_retrieving_items():
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = "The first (ever) list item"
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = "Item the second"
+    second_item.list = list_
     second_item.save()
+
+    saved_list = List.objects.first()
+    assert saved_list == list_
 
     saved_items = Item.objects.all()
     assert saved_items.count() == 2
     assert saved_items[0].text == "The first (ever) list item"
+    assert saved_items[0].list == list_
     assert saved_items[1].text == "Item the second"
+    assert saved_items[1].list == list_
 
 
 # List view
@@ -33,8 +43,9 @@ def test_uses_list_template(client):
 
 
 def test_displays_all_items(client):
-    Item.objects.create(text="itemey 1")
-    Item.objects.create(text="itemey 2")
+    list_ = List.objects.create()
+    Item.objects.create(text="itemey 1", list=list_)
+    Item.objects.create(text="itemey 2", list=list_)
 
     response = client.get("/lists/the-only-list-in-the-world/")
 
