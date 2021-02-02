559af01a28d973eedf3b5a0485cdb0fd58f90c9f
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-02-02 15:30:39 +0000

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -95,6 +95,29 @@ def test_multiple_users_can_start_lists_at_different_urls(live_server, driver_fa
     # Satisfied, they both go back to sleep
 
 
+@pytest.mark.functest
+def test_layout_and_stying(live_server, selenium):
+    # Edith goes to the home page
+    selenium.get(str(live_server))
+    selenium.set_window_size(1024, 768)
+
+    # She notices the input box is nicely centered
+    inputbox = selenium.find_element_by_id("id_new_item")
+    assert inputbox.location["x"] + inputbox.size["width"] / 2 == pytest.approx(
+        512, rel=10
+    )
+
+    # She starts a new list and sees the input is nicely
+    # centered there too
+    inputbox.send_keys("testing")
+    inputbox.send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(selenium, "1: testing")
+    inputbox = selenium.find_element_by_id("id_new_item")
+    assert inputbox.location["x"] + inputbox.size["width"] / 2 == pytest.approx(
+        512, rel=10
+    )
+
+
 # Helpers
 def wait_for_row_in_list_table(browser, row_text):
     start_time = time.time()
