aa298c1c4a0574c0b8f1e29e321c629c430bf035
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-03-14 22:27:35 +0000

    split unit tests into views and models files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -15,32 +15,6 @@ def test_home_page_returns_correct_html(client):
     assertTemplateUsed(response, "home.html")
 
 
-# List and Item model
-def test_saving_and_retrieving_items():
-    list_ = List()
-    list_.save()
-
-    first_item = Item()
-    first_item.text = "The first (ever) list item"
-    first_item.list = list_
-    first_item.save()
-
-    second_item = Item()
-    second_item.text = "Item the second"
-    second_item.list = list_
-    second_item.save()
-
-    saved_list = List.objects.first()
-    assert saved_list == list_
-
-    saved_items = Item.objects.all()
-    assert saved_items.count() == 2
-    assert saved_items[0].text == "The first (ever) list item"
-    assert saved_items[0].list == list_
-    assert saved_items[1].text == "Item the second"
-    assert saved_items[1].list == list_
-
-
 # List view
 def test_uses_list_template(client):
     list_ = List.objects.create()
