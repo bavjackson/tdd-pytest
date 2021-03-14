fdf8680d6dc981f7d547d2d55383bfd838282aa8
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-03-14 21:42:29 +0000

    made debug mode conditional on environment variable

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "g4wch+d2_df&w-=myutgqbk-=qizye(2*xdubgg8+!!^(&8$m^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
